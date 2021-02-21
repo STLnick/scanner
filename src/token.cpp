ccb6673ed6a17a5b8416dad8113d28e2cfd4a678
Nick <nickrray@gmail.com>
2021-02-21 17:37:27 -0600

    Remove comments

--- src/token.cpp
+++ src/token.cpp
@@ -1,12 +1,6 @@
 #include <string>
 #include "TokenRecord.hpp"
 
-/**
- * @param name string value of the token
- * @param tokenId the type of the token
- * @param lineNum the line number the token is on
- * @return newly instantiated TokenRecord
- */
 TokenRecord *initToken(std::string name, tokenID tokenId, int lineNum) {
     TokenRecord *newToken;
     newToken = (TokenRecord *) malloc(1 * sizeof(TokenRecord));
