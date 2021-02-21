87f6b962f01ce0c68685b2f8a596f675ade548fa
Nick <nickrray@gmail.com>
2021-02-21 17:03:22 -0600

    Setup struct for TokenRecord with type enum

---
+++ include/TokenRecord.hpp
@@ -0,0 +1,27 @@
+/*
+ * Nick Ray
+ * 2/15/21
+ */
+
+#ifndef TOKEN_RECORD_HPP
+#define TOKEN_RECORD_HPP
+
+#include <string>
+
+//   =     =>   =<  ==   : := + - * / % . ( ) , { } ; [ ]
+// assign gteq lteq eq 
+
+// TODO: Make these TokenTypes the actual needed types
+enum tokenID {
+    ID_tk, NUM_tk
+};
+const std::string tokenNames[] =
+        {"Identifier", "Number"};
+
+typedef struct {
+    tokenID tokenId;       // The token type
+    std::string stringVal; // The identifying string value for the token
+    int lineNum;           // Line number which the token is on in source
+} TokenRecord;
+
+#endif /* TOKEN_RECORD_HPP */
